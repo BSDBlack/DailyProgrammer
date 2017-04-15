a5b8d9466c5b7f337c0edd1b6ade01330350196e
Kreuzer, Sebastian <sebastian.kreuzer@student.fhws.de>
2017-04-15 12:03:18 +0200

    refactor

---
+++ Problem_286e/reverseFaculty.c
@@ -0,0 +1,46 @@
+/*
+ * reverseFaculty.c
+ *
+ *  Created on: Oct 8, 2016
+ *      Author: Sebastian Kreuzer
+ */
+
+/*
+ * [2016-10-03] Challenge #286 [Easy] Reverse Factorial
+ */
+
+/*
+ * int reverseFactorial(int number)
+ *
+ * int number - takes argument of number to test
+ * returns - factorial, if none, -1
+ */
+
+int reverseFactorial(int number)
+{
+	// variable declaration
+	int faculty = 1;
+	int isFaculty = 1;
+
+	// code
+	do {
+		++faculty;
+
+		if(0 != number % faculty)
+		{
+			isFaculty = 0;
+		}
+		else
+		{
+			number /= faculty;
+		}
+
+	} while(1 == isFaculty && number >= faculty);
+
+	if(0 == isFaculty)
+	{
+		faculty = -1;
+	}
+
+	return faculty;
+}
---
+++ Problem_286e/reverseFaculty.h
@@ -0,0 +1,13 @@
+/*
+ * reverseFaculty.h
+ *
+ *  Created on: Oct 8, 2016
+ *      Author: bsdblack
+ */
+
+#ifndef REVERSEFACULTY_H_
+#define REVERSEFACULTY_H_
+
+int reverseFaculty(int number);
+
+#endif /* REVERSEFACULTY_H_ */
